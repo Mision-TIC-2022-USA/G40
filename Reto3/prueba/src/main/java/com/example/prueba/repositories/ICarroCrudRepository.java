7dea60d1fba321b2c208cdd2eee710ea7089d6eb
Angel Morales <angelmoralesb@gmail.com>
2021-10-29 22:11:07 -0500

    nuevo ICarroCrudRepository

---
+++ Reto3/prueba/src/main/java/com/example/prueba/repositories/ICarroCrudRepository.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.example.prueba.repositories;
+
+import com.example.prueba.models.CarroModel;
+import com.example.prueba.models.ClienteModel;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author ASUS RYZEN
+ */
+public interface ICarroCrudRepository extends CrudRepository<CarroModel, Integer>{
+    
+}
